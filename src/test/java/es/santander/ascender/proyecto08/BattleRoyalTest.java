02e1d1120870945afc2d99e3d4a1dfbbdf14e80b
curso00 <curso00@nomail.com>
2024-11-22 12:56:17 +0100

    Ejemplo de Streams

--- src/test/java/es/santander/ascender/proyecto08/BattleRoyalTest.java
+++ src/test/java/es/santander/ascender/proyecto08/BattleRoyalTest.java
@@ -1,6 +1,11 @@
 package es.santander.ascender.proyecto08;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 
@@ -21,6 +26,23 @@ public class BattleRoyalTest {
         }
 
         a.getRandomDiaSemana();
+    }
+
+    @Test
+    public void testStream() {
+        List<String> lista = Arrays.asList("Hola", "Adios", "Hi", "Goodbye");
+        
+        int suma = lista.stream()
+                .mapToInt(s -> s.length())
+                .filter(x -> x >= 5)
+                .sum();
+
+        assertEquals(12, suma);
+
+        List<String> cadenasCortas = lista.stream()
+                .filter(s -> s.length() < 5)
+                .collect(Collectors.toList());
+        assertEquals(2, cadenasCortas.size());
     }
 
 }
